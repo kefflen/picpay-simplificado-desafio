e6f80c76b6a6e1d5b170cc4f86228631355ce734
kefflen <kefflenjava@hotmail.com>
2024-03-24 15:33:54 -0300

    feat: create TransactionController with post method

---
+++ src/main/java/com/github/kefflen/picpaysimplificado/transaction/TransactionController.java
@@ -0,0 +1,21 @@
+package com.github.kefflen.picpaysimplificado.transaction;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/transaction")
+public class TransactionController {
+    private final TransactionService transactionService;
+
+    public TransactionController(TransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
+
+    @PostMapping
+    public Transaction create(@RequestBody Transaction transaction) {
+        return transactionService.create(transaction);
+    }
+}

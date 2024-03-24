01f103b57b26dddf328297ebe5c68399751ed12d
kefflen <kefflenjava@hotmail.com>
2024-03-24 15:57:22 -0300

    fix: create topic created:transaction

--- src/main/java/com/github/kefflen/picpaysimplificado/PicpaySimplificadoApplication.java
+++ src/main/java/com/github/kefflen/picpaysimplificado/PicpaySimplificadoApplication.java
@@ -1,8 +1,11 @@
 package com.github.kefflen.picpaysimplificado;
 
+import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jdbc.repository.config.EnableJdbcAuditing;
+import org.springframework.kafka.config.TopicBuilder;
 
 @EnableJdbcAuditing
 @SpringBootApplication
@@ -11,4 +14,11 @@ public class PicpaySimplificadoApplication {
         SpringApplication.run(PicpaySimplificadoApplication.class, args);
     }
 
+    @Bean
+    NewTopic notificationTopic() {
+        return TopicBuilder.name("created:transaction")
+                .partitions(1)
+                .replicas(1)
+                .build();
+    }
 }

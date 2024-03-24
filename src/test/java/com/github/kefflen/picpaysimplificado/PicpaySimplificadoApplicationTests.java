cef48731b82b4a7998f5d379381670a8c1bff878
kefflen <kefflenjava@hotmail.com>
2024-03-24 13:55:47 -0300

    chore: initialize spring boot project

---
+++ src/test/java/com/github/kefflen/picpaysimplificado/PicpaySimplificadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.kefflen.picpaysimplificado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpaySimplificadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
